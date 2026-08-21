3cd657fcbbaa85b92700df8c20f9fe99e35f810d
Murilo <murilo.bap.oliv@gmail.com>
2026-08-21 17:18:43 -0300

    feat: traduz MethodNotValidException para HTTP 400

--- backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
+++ backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
@@ -2,14 +2,38 @@ package br.com.athenassys.api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<String> tratarErros(EntidadeNaoEncontradaException ex) {
+    public ResponseEntity<String> tratarErro404(EntidadeNaoEncontradaException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
+
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    record DadosErroValidacao(
+            String campo,
+            String mensagem
+    ) {
+        public DadosErroValidacao(FieldError erro) {
+            this(
+                    erro.getField(),
+                    erro.getDefaultMessage()
+            );
+        }
     }
 }
