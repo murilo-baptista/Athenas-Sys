4f35fa2b6e5e065057abd31f5cc64a603a43889c
Murilo <murilo.bap.oliv@gmail.com>
2026-08-21 17:50:35 -0300

    feat: traduz HttpRequestMethodNotSupportedException

--- backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
+++ backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
@@ -3,9 +3,12 @@ package br.com.athenassys.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.MethodNotAllowedException;
 
 import java.util.List;
 
@@ -23,6 +26,17 @@ public class TratadorDeErros {
         var erros = ex.getFieldErrors();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity tratarErro405(HttpRequestMethodNotSupportedException ex) {
+
+        var metodo = ex.getMethod();
+        var suportados = ex.getSupportedHttpMethods();
+
+        var mensagemErro = "Requisição '" + metodo + "' não suportada para essa rota. \nMétodos suportados: " + suportados;
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(mensagemErro);
     }
 
     record DadosErroValidacao(
