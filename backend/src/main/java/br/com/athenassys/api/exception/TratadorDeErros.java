dafae5ca0aae05c95a452483c1cfa5f4d193ba0b
Murilo <murilo.bap.oliv@gmail.com>
2026-08-22 13:55:35 -0300

    refactor: remove imports mortos

--- backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
+++ backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
@@ -7,8 +7,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.server.MethodNotAllowedException;
 
 import java.util.List;
 
