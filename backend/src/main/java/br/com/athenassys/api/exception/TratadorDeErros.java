88a3267e329e017ee27c5594c41203cb6d8102e3
Murilo <murilo.bap.oliv@gmail.com>
2026-08-22 13:53:40 -0300

    refactor: Comenta o código

--- backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
+++ backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
@@ -15,11 +15,13 @@ import java.util.List;
 @RestControllerAdvice
 public class TratadorDeErros {
 
+    // Trata erro 404 para Entidades do Banco de Dados não existentes
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<String> tratarErro404(EntidadeNaoEncontradaException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    // Trata erro 400 para campos preenchidos incorretamente
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
 
@@ -28,6 +30,7 @@ public class TratadorDeErros {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
+    // Trata erro 405 para Requisições HTTP não suportadas
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity tratarErro405(HttpRequestMethodNotSupportedException ex) {
 
@@ -39,6 +42,8 @@ public class TratadorDeErros {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(mensagemErro);
     }
 
+
+    // Transforma a mensagem de erro do 405 para facilitar visualização
     record DadosErroValidacao(
             String campo,
             String mensagem
