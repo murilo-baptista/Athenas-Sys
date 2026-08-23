d4505624bccc34056183395efb441cba2daf13d2
ZK-Tshz <murilo.bap.oliv@gmail.com>
2026-08-22 23:52:22 -0300

    feat: implementa tratamento do erro 404 NoResourceFoundException

--- backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
+++ backend/src/main/java/br/com/athenassys/api/exception/TratadorDeErros.java
@@ -1,5 +1,7 @@
 package br.com.athenassys.api.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -7,6 +9,7 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
 
@@ -30,7 +33,7 @@ public class TratadorDeErros {
 
     // Trata erro 405 para Requisições HTTP não suportadas
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity tratarErro405(HttpRequestMethodNotSupportedException ex) {
+    public ResponseEntity<String> tratarErro405(HttpRequestMethodNotSupportedException ex) {
 
         var metodo = ex.getMethod();
         var suportados = ex.getSupportedHttpMethods();
@@ -40,6 +43,22 @@ public class TratadorDeErros {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(mensagemErro);
     }
 
+    // Trata erro 404 para erros de digitação com barras
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<String> tratarErroDigitacao(NoResourceFoundException ex, HttpServletRequest request) {
+
+        var ultimoCaractere = request.getRequestURI().charAt(request.getRequestURI().length() - 1);
+        var caminho = "/" + ex.getResourcePath();
+        var parametros = request.getQueryString();
+
+        if (ultimoCaractere == '/') {
+            var url = (parametros != null) ? caminho + "?" + parametros : caminho;
+            return ResponseEntity.status(308).header(HttpHeaders.LOCATION, url).build();
+        }
+        return ResponseEntity.status(404).body(
+                "404 Página Não Encontrada! \nPoxa! Não foi possível encotrar a página! \nO link pode estar quebrado, inacessível ou ter sido digitado de maneira incorreta."
+        );
+    }
 
     // Transforma a mensagem de erro do 405 para facilitar visualização
     record DadosErroValidacao(
