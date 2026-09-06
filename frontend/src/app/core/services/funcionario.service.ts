e60bca77e75b032ec3226693cea5219ad75f0c65
ZK-Tshz <murilo.bap.oliv@gmail.com>
2026-09-06 19:20:17 -0300

    fix (frontend): corrige importações de Pedido e Funcionario.

--- frontend/src/app/core/services/funcionario.service.ts
+++ frontend/src/app/core/services/funcionario.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { CriarFuncionarioRequest, Funcionario } from '../models/funcionario.model';
+import { CriarFuncionarioRequest, AtualizarFuncionarioRequest, Funcionario } from '../models/funcionario.model';
 
 @Injectable({ providedIn: 'root' })
 export class FuncionarioService {
--- frontend/src/app/core/services/pedido.service.ts
+++ frontend/src/app/core/services/pedido.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { AtualizarStatusPedidoRequest, CriarPedidoRequest, Pedido, StatusPedido } from '../models/pedido.model';
+import { AtualizarPedidoRequest, CriarPedidoRequest, Pedido} from '../models/pedido.model';
 
 @Injectable({ providedIn: 'root' })
 export class PedidoService {
