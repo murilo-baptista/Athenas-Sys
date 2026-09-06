9050f2cff3f1532d258cdc8a00315e56578bbe59
ZK-Tshz <murilo.bap.oliv@gmail.com>
2026-09-06 19:29:42 -0300

    feat (frontend): alinha totalmente as rotas de Restaurante ao backend.

--- frontend/src/app/core/services/restaurante.service.ts
+++ frontend/src/app/core/services/restaurante.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { CadastroRestauranteRequest, Restaurante } from '../models/restaurante.model';
+import { CadastroRestauranteRequest, AtualizarRestauranteRequest, Restaurante } from '../models/restaurante.model';
 
 @Injectable({ providedIn: 'root' })
 export class RestauranteService {
@@ -13,6 +13,18 @@ export class RestauranteService {
 
   cadastrar(dados: CadastroRestauranteRequest): Observable<Restaurante> {
     return this.http.post<Restaurante>(this.baseUrl, dados);
+  }
+
+  buscar(): Observable<Restaurante> {
+    return this.http.get<Restaurante>(`${this.baseUrl}`);
+  }
+
+  atualizar(id: number, dados: AtualizarRestauranteRequest): Observable<Restaurante> {
+    return this.http.put<Restaurante>(`${this.baseUrl}/${id}`, dados)
+  }
+
+  remover(id: number): Observable<Restaurante> {
+    return this.http.delete<Restaurante>(`${this.baseUrl}/${id}`) 
   }
 
   buscarPorId(id: number): Observable<Restaurante> {
