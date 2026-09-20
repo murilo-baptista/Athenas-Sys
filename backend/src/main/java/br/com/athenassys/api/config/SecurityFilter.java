6ef30f3a78a90ce64d9c13d9fa8db6ee3da2e546
ZK-Tshz <murilo.bap.oliv@gmail.com>
2026-09-20 15:14:14 -0300

    feat: implementa filtro de autenticação de restaurante
    Agora toda e qualquer requisição que não tenha a rota de login (/auth/login ou /auth/funcionario/login), é bloqueada, a menos que contenha um header Authorization com o token JWT gerado nas requisições de login.
    
    Ainda não implementado o filtro de autenticação para os funcionários.

---
+++ backend/src/main/java/br/com/athenassys/api/config/SecurityFilter.java
@@ -0,0 +1,51 @@
+package br.com.athenassys.api.config;
+
+import br.com.athenassys.api.repository.RestauranteRepository;
+import br.com.athenassys.api.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var tokenJWT = recuperarToken(request);
+
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var restaurante = restauranteRepository.findByEmail(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(restaurante, null, restaurante.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authoriztionHeader = request.getHeader("Authorization");
+
+        if (authoriztionHeader != null) {
+            return authoriztionHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
--- backend/src/main/java/br/com/athenassys/api/service/TokenService.java
+++ backend/src/main/java/br/com/athenassys/api/service/TokenService.java
@@ -3,6 +3,7 @@ package br.com.athenassys.api.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,21 @@ public class TokenService {
 
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token JWT ", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+
+            return JWT.require(algoritimo)
+                    .withIssuer("API Athenas Sys")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
